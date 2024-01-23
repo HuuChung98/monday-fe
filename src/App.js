c33d58e4dddd21098210aaa8251a06d3c9b84352
HuuChung98 <balechung93@gmail.com>
2024-01-23 22:17:31 +0700

    create sign up module

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
+import SignUp from './pages/auth/signUp/SignUp';
+
+
 function App() {
   return (
     <>
-      <Routes></Routes>
+      <Routes>
+        <Route path='sign-up' element={<SignUp></SignUp>}></Route>
+      </Routes>
     </>
   );
 }
---
+++ src/pages/auth/signUp/SignUp.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <div>
+        <div className='row d-flex'>
+            <div className='wellcome-signUp col-sm-7 bg-success'>
+                <div>
+                    <button type="button" 
+                        aria-label="Continue with Google" 
+                        aria-pressed="false"><img class="Q2rN3" 
+                        alt="Continue with Google" 
+                        src="https://dapulse-res.cloudinary.com/image/upload/remote_logos/995426/google-icon.svg"/>
+                        <span>Continue with Google</span>
+                    </button>
+                </div>
+
+            </div>
+            <div className='background-form col-sm-5 bg-warning'>
+                <img class="O1nZl" src="https://dapulse-res.cloudinary.com/image/upload/monday_platform/signup/signup-right-side-assets-new-flow/welcome-to-monday.png" alt="Background" data-testid="right-side-asset"/>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default SignUp
