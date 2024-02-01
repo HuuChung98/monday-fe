cf9c0041e521b2f486fba5e7033cbfd706dfccd2
HuuChung98 <balechung93@gmail.com>
2024-02-01 15:40:51 +0700

    create inviteMember component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import SignUp from './pages/auth/signUp/SignUp';
 import CreateAccount from './pages/auth/signUp/CreateAccount';
+import InviteMembers from './pages/auth/signUp/InviteMembers';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='sign-up' element={<SignUp></SignUp>}></Route>
         <Route path='users/invitation/accept' element={<CreateAccount></CreateAccount>}></Route>
+        <Route path='users/invitation/member' element={<InviteMembers></InviteMembers>}></Route>
       </Routes>
     </>
   );
---
+++ src/pages/auth/signUp/InviteMembers.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const InviteMembers = () => {
+  return (
+    <div>InviteMembers</div>
+  )
+}
+export default InviteMembers
